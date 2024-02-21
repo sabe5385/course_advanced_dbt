9db0c533b1855c8a8a4881df7fd28950aae476fe
Sara Berglund <sara.im.berglund@gmail.com>
2024-02-21 08:48:51 +0000

    Task 3 - week 2

---
+++ macros/projects/README_week4.md
@@ -0,0 +1,9 @@
+Week 4 - Project Notes
+
+Task 1:
+Fixed incremental model for bingeflix events, and added a datespan on minus 3 days.
+
+Task 2:
+4 Unfortunately I am not able to reach my snowflake account for some reason, it just says that an error occured when signing in.
+
+Task 3:
--- models/marts/finance/fct_mrr.sql
+++ models/marts/finance/fct_mrr.sql
@@ -6,21 +6,8 @@
 WITH
 
 -- Import CTEs
--- Get raw monthly subscriptions
-monthly_subscriptions AS (
-    SELECT
-        subscription_id,
-        user_id,
-        starts_at,
-        ends_at,
-        plan_name,
-        pricing,
-        {{ trunc_to_month('starts_at') }} AS start_month,
-        {{ trunc_to_month('ends_at') }} AS end_month
-    FROM
-        {{ unit_testing_select_table(ref('dim_subscriptions'), ref('unit_test_input_dim_subscription_mrr')) }}
-    WHERE
-        billing_period = 'monthly'
+subscription_periods AS (
+    SELECT * FROM {{ ref('int_subscription_periods') }}
 ),
 
 -- Use the dates spine to generate a list of months
@@ -31,29 +18,6 @@ months AS (
         {{ unit_testing_select_table(ref('int_dates'), ref('unit_test_input_dim_dates_mrr')) }}
     WHERE
         day_of_month = 1
-),
-
--- Logic CTEs
--- Create subscription period start_month and end_month ranges
-subscription_periods AS (
-    SELECT
-        subscription_id,
-        user_id,
-        plan_name,
-        pricing AS monthly_amount,
-        starts_at,
-        ends_at,
-        start_month,
-
-        -- For users that cancel in the first month, set their end_month to next month because the subscription remains active until the end of the first month
-        -- For users who haven't ended their subscription yet (end_month is NULL) set the end_month to one month from the current date (these rows will be removed from the final CTE)
-        CASE
-            WHEN start_month = end_month THEN DATEADD('month', 1, end_month)
-            WHEN end_month IS NULL THEN DATE(DATEADD('month', 1, DATE_TRUNC('month', CURRENT_DATE)))
-            ELSE end_month
-        END AS end_month
-    FROM
-        monthly_subscriptions
 ),
 
 -- Determine when given subscription plan's first and most recent months
